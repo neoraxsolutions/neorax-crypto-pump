c35c59b5c388c137af84ae1332370da81154440b
neoraxsolutions <info@neoraxsolutions.com>
2025-07-13 15:14:24 -0700

    sendAlert.js

---
+++ sendAlert.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+
+const botToken = process.env.TELEGRAM_BOT_TOKEN;
+const chatId = process.env.TELEGRAM_CHAT_ID;
+
+function sendPumpAlert(coin, price) {
+  const message = `🚀 Pump Alert!\nCoin: ${coin}\nPrice: $${price}\n#NeoraxCryptoPump`;
+  const url = `https://api.telegram.org/bot${botToken}/sendMessage`;
+
+  return axios.post(url, {
+    chat_id: chatId,
+    text: message
+  });
+}
+
+module.exports = sendPumpAlert;
